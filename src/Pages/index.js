b3b4339f547ecd4db0788dfe0792474e11f1f4f9
Pavan Kalyan Reddy Konudula <pkonudul@uncc.edu>
2024-04-06 19:46:53 -0400

    removed pages export

--- src/Pages/index.js
+++
@@ -1,9 +0,0 @@
-import LandingPage from "./LandingPage/LandingPage";
-import ProfessorProfilePage from "./ProfessorProfilePage/ProfessorProfilePage";
-
-const Pages ={
-    LandingPage:LandingPage,
-    ProfessorProfilePage:ProfessorProfilePage
-}
-
-export default Pages;

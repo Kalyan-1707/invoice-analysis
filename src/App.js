48b9a7a4a3740e18b7342fcccbfd6a7cf9c25f8f
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-04-06 16:06:45 -0400

    routers added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import React from "react";
 import React, { useEffect, useState } from "react";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 import "./App.css";
-
 import Pages from "./Pages";
 
-
 const App = () => {
-
-    return (
-        <Pages.LandingPage/>
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Pages.LandingPage />}></Route>
+        {/* <Route path="/student"></Route> */}
+        <Route path="/professor/:id"></Route>
+        <Route path="/applications"></Route>
+        <Route path="/job-posting"></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;

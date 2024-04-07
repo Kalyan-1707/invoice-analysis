d7acba80489103234dbd2a9ebc62f757deae39d6
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-04-06 22:20:47 -0400

    added applications page

--- src/App.js
+++ src/App.js
@@ -12,19 +12,19 @@ import "./App.css";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import ProfessorProfilePage from "./Pages/ProfessorProfilePage/ProfessorProfilePage";
 import StudentProfile from "./Pages/StudentProfile/StudentProfile";
+import Applcations from "./Pages/StudentProfile/applcations";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />}></Route>
-        <Route path="/student" element={<StudentProfile />}></Route>
-        <Route path="/professor" element={<ProfessorProfilePage />}></Route>
-        {/* <Route path="/professor/:id"></Route>
-        <Route path="/applications"></Route>
-        <Route path="/job-posting"></Route> */}
-        <Route path="*" element={<div>Page not found</div>}></Route>
-
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/student/profile/edit" element={<StudentProfile />} />
+        <Route path="/professor" element={<ProfessorProfilePage />} />
+        <Route path="/student" element={<Applcations />} />
+        {/* <Route path="/professor/:id"/>
+        <Route path="/job-posting"/> */}
+        <Route path="*" element={<div>Page not found</div>} />
       </Routes>
     </Router>
   );
